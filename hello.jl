0126689eca85d6afbda3e2dde88abd6e9e4defa8
JunyaIto <ito.23571113@gmail.com>
2025-04-17 15:35:17 +0900

    tokuninasi

---
+++ hello.jl
@@ -0,0 +1,30 @@
+function add(a, b)
+    c = a + b
+    return c 
+end
+
+function main()
+    a = 5
+    b = 8
+    println(add(a,b))
+end
+
+main()
+
+println("Hello, World!")
+println("This is a test of the Julia language.")
+
+function test(g, h)
+    i = (g * h) ^ 2
+    return i
+end
+
+function subu()
+    g = 5
+    h = 2
+    println(test(g, h))
+end
+
+subu()
+
+iroiroaru
